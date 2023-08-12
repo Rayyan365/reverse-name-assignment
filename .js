0ea0751872cd6578aa374133370297d96560d292
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-12 09:39:01 -0700

    reverse-name assignment done

---
+++ .js
@@ -0,0 +1,6 @@
+let names=[ "Aisha","Ahmed","Hassan","Faadumo","Khaalid","Jammac","Asma"]
+
+console.log(names.lenght);
+for(let i=0; i>names.length;i--){
+    console.log( `my name is ${names[i]}`)
+}
